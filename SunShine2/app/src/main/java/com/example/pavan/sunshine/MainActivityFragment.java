fd53bd0a8fd6ce1a3013543f5261216fd9e04210
Sameera S <sameera1595@gmail.com>
2016-02-04 21:25:42 +0530

    completed json

--- SunShine2/app/src/main/java/com/example/pavan/sunshine/MainActivityFragment.java
+++ SunShine2/app/src/main/java/com/example/pavan/sunshine/MainActivityFragment.java
@@ -13,6 +13,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,6 +26,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.logging.Logger;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -45,13 +54,16 @@ public class MainActivityFragment extends Fragment {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_refresh) {
-            AsyncTask<Void, Void, Void> execute = new FetchTask();
-            execute.execute();
+            FetchTask exec = new FetchTask();
+            exec.execute("560040");
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -61,6 +73,7 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         container = (ViewGroup) inflater.inflate(R.layout.fragment_main, container, false);
 
         ArrayList<String> myarray = new ArrayList<>();
@@ -72,6 +85,8 @@ public class MainActivityFragment extends Fragment {
         myarray.add("fri-Sunny-88/63");
         myarray.add("sat-Sunny-88/63");
 
+
+
         ArrayAdapter<String> myadpt = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, myarray);
         ListView lv = (ListView) container.findViewById(R.id.listview_forcast);
         lv.setAdapter(myadpt);
@@ -81,22 +96,24 @@ public class MainActivityFragment extends Fragment {
 
 
 
-    public class FetchTask extends AsyncTask<Void, Void,Void> {
+    public class FetchTask extends AsyncTask<String, Void,Void> {
 
         // These two need to be declared outside the try/catch
 // so that they can be closed in the finally block.
+        String[] str = new String[7];
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
             Uri.Builder builder = new Uri.Builder();
-            //http://api.openweathermap.org/data/2.5/find?q=560040&type=like&mode=json&units=metric&cnt=7&appid=41a404f10522ec4c2eea0dbeb272836f
-            builder.scheme("http").authority("api.openweathermap.org").appendPath("data").appendPath("find").appendQueryParameter("q","560040")
-                    .appendQueryParameter("type","like").appendQueryParameter("mode","json").appendQueryParameter("units","metrics")
-                    .appendQueryParameter("cnt","7").appendQueryParameter("appid","41a404f10522ec4c2eea0dbeb272836f");
+            String postcode = params[0]+ ",india";
+            //http://api.openweathermap.org/data/2.5/forecast/daily?zip=560040,india&mode=json&units=metric&cnt=7&appid=41a404f10522ec4c2eea0dbeb272836f
+            builder.scheme("http").authority("api.openweathermap.org").appendPath("data").appendPath("2.5").appendPath("forecast").appendPath("daily").appendQueryParameter("zip", postcode)
+                    .appendQueryParameter("type", "like").appendQueryParameter("mode", "json").appendQueryParameter("units", "metric")
+                    .appendQueryParameter("cnt", "7").appendQueryParameter("appid","41a404f10522ec4c2eea0dbeb272836f");
 
             HttpURLConnection urlConnection = null;
             BufferedReader reader = null;
-
+            Log.e("the url ",builder.toString());
             // Will contain the raw JSON response as a string.
             String forecastJsonStr = null;
 
@@ -106,7 +123,7 @@ public class MainActivityFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are available at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+                URL url = new URL(builder.toString());
 
                 // Create the request to OpenWeatherMap, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
@@ -135,6 +152,12 @@ public class MainActivityFragment extends Fragment {
                     forecastJsonStr = null;
                 }
                 forecastJsonStr = buffer.toString();
+                Log.e("the reply", forecastJsonStr);
+                parseths(forecastJsonStr);
+                for(String si : str){
+                    Log.e("the day  ",si);
+                }
+
             }
 
             catch(
@@ -146,9 +169,9 @@ public class MainActivityFragment extends Fragment {
                 // If the code didn't successfully get the weather data, there's no point in attempting
                 // to parse it.
                 forecastJsonStr = null;
-            }
-
-            finally
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally
 
             {
                 if (urlConnection != null) {
@@ -165,6 +188,46 @@ public class MainActivityFragment extends Fragment {
 
             return null;
         }
+
+        private void parseths(String forecastJsonStr) throws JSONException {
+
+            //int day = calendar.get(Calendar.DATE);
+            //Date date;// calendar.getTime();
+            //Date date = calendar.getTime();
+
+            for(int i=0;i<7;i++){
+                Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+                Date date = calendar.getTime();
+                calendar.setTime(date);
+                calendar.add(Calendar.DATE, i);
+                date = calendar.getTime();
+                String mys = date.toString();
+                mys = mys.substring(0, 10);
+                StringBuilder myName = new StringBuilder(mys);
+                myName.setCharAt(3, ',');
+                mys = myName.toString();
+                String desc = getDesc(forecastJsonStr,i);
+                 mys = mys + desc;
+                str[i] = mys;
+            }
+
+        }
+        //max = arr.getJSONObject(dayIndex).getJSONObject("temp").getDouble("max");
+        //min = arr.getJSONObject(dayIndex).getJSONObject("temp").getDouble("min");
+
+        private String getDesc(String forecastJsonStr, int dayIndex) throws JSONException {
+            JSONObject obj = new JSONObject(forecastJsonStr);
+            JSONArray arr = obj.getJSONArray("list");
+            JSONArray s = arr.getJSONObject(dayIndex).getJSONArray("weather");
+
+            String post_id = " - " +s.getJSONObject(0).getString("main");
+            int min =(int) arr.getJSONObject(dayIndex).getJSONObject("temp").getDouble("min");
+
+            post_id = post_id +" - "+ (int)arr.getJSONObject(dayIndex).getJSONObject("temp").getDouble("max")+'/' +min;
+
+            return post_id;
+        }
+
 
     }
 }
