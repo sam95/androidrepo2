2632b231149b0dbfe4ffdbe614a5adb428262dc1
Sameera S <sameera1595@gmail.com>
2016-02-13 19:16:00 +0530

    share added

--- SunShine2/app/src/main/java/com/example/pavan/sunshine/DetailActivity.java
+++ SunShine2/app/src/main/java/com/example/pavan/sunshine/DetailActivity.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    private ShareActionProvider sap;
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,7 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
@@ -25,6 +32,21 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+        MenuItem mi = menu.findItem(R.id.shareid);
+
+        sap = (ShareActionProvider) MenuItemCompat.getActionProvider(mi);
+        Intent iu = new Intent(Intent.ACTION_SEND);
+        TextView tv = (TextView) findViewById(R.id.my_weat);
+        data = tv.getText().toString();
+        iu.setType("text/plain");
+        Log.e("hello", data);
+        iu.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        iu.putExtra(Intent.EXTRA_TEXT, data + " #Sunshineapp");
+        if(sap!=null) {
+            Log.e("uclicked", "share button");
+            sap.setShareIntent(iu);
+            //startActivity(iu);
+        }
         return true;
     }
 
--- SunShine2/app/src/main/java/com/example/pavan/sunshine/DetailActivityFragment.java
+++ SunShine2/app/src/main/java/com/example/pavan/sunshine/DetailActivityFragment.java
@@ -1,10 +1,16 @@
 package com.example.pavan.sunshine;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,13 +21,27 @@ import android.widget.TextView;
  */
 public class DetailActivityFragment extends Fragment {
 
+    private ShareActionProvider sap;
+    String data;
     public DetailActivityFragment() {
+        //setHasOptionsMenu(true);
     }
 
 
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.menu_detail, menu);
+        /*MenuItem mi = menu.findItem(R.id.shareid);
+        sap = (ShareActionProvider) MenuItemCompat.getActionProvider(mi);
+        Intent iu = new Intent(Intent.ACTION_SEND);
+        iu.setType("text/plain");
+        iu.putExtra(Intent.EXTRA_TEXT, data + " #Sunshineapp");
+        if(sap!=null) {
+            Log.e("uclicked","share button");
+            sap.setShareIntent(iu);
+            startActivity(iu);
+        }*/
     }
 
     @Override
@@ -44,7 +64,7 @@ public class DetailActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         container = (ViewGroup) inflater.inflate(R.layout.fragment_detail, container, false);
         TextView tv  = (TextView) container.findViewById(R.id.my_weat);
-        String data = getActivity().getIntent().getExtras().getString("data");
+        data = getActivity().getIntent().getExtras().getString("data");
         tv.setText(data);
         return container;
     }
